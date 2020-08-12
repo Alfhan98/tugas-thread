ddde1fe0b055e7ef8a301d597936e6404b5cc178
Alfhan98 <68676112+Alfhan98@users.noreply.github.com>
2020-08-12 16:53:26 +0800

    Add files via upload

---
+++ looping/src/looping/cnth_dowhile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author acer
+ */
+public class cnth_dowhile {
+public static void main(String[] args) {
+
+        // membuat variabel
+        int i = 1;
+
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while ( i <= 10);
+
+    }
+}
---
+++ looping/src/looping/cnth_for.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package looping;
+
+/**
+ *
+ * @author acer
+ */
+public class cnth_for {
+    public static void main(String[] args) {
+        String kamu = "Hello";
+        int jumlah = 5;
+        for(int x=0; x<jumlah; x++){
+            System.out.println(kamu);
+        }
+    }
+}
+    
+    
+
